2f059fac80cb9d198e65cbdcf0b8ff00d9929d59
Marc Betoule <betoule@lpnlp202219.in2p3.fr>
2024-06-21 16:07:51 +0200

    First version

--- cbp_paper_project/authors.py
+++ cbp_paper_project/authors.py
@@ -1,36 +1,34 @@
 spieman = r'''
 \author[a,*]{Thierry Souverin}
-\author[b,g]{Jérémy Neveu}
+\author[a,b]{Jérémy Neveu}
 \author[a]{Marc Betoule}
 \author[a]{Sébastien Bongard}
 \author[g]{Christopher W. Stubbs}
 \author[g]{Elana Urbach}
 \author[g]{Sasha Brownsberger}
-\author[d]{Pierre Éric Blanc}
+\author[c]{Pierre Éric Blanc}
 \author[e,f]{Johann Cohen Tanugi}
 \author[b]{Sylvie Dagoret-Campagne}
-\author[c]{Fabrice Feinstein}
+\author[d]{Fabrice Feinstein}
 \author[a]{Delphine Hardin}
 \author[a]{Claire Juramy}
 \author[a]{Laurent Le Guillou}
-\author[d]{Auguste Le Van Suu}
+\author[c]{Auguste Le Van Suu}
 \author[b]{Marc Moniez}
 \author[e]{Bertrand Plez}
 \author[a]{Nicolas Regnault}
 \author[a]{Eduardo Sepulveda}
 \author[e]{Kélian Sommer}
 
-
-
 \affil[a]{LPNHE, CNRS/IN2P3 \& Sorbonne Université, 4 place Jussieu, 75005 Paris, France}
 \affil[b]{Universit\'e Paris-Saclay, CNRS, IJCLab, 91405, Orsay, France}
-\affil[c]{Aix Marseille Univ, CNRS/IN2P3, CPPM, Marseille, France}
-\affil[d]{Université d’Aix-Marseille \& CNRS, Observatoire de Haute-Provence, 04870 Saint Michel l’Observatoire, France}
+\affil[g]{Department of Astronomy, Harvard University, 60 Garden St., Cambridge, MA 02138, USA}
+\affil[c]{Université d’Aix-Marseille \& CNRS, Observatoire de Haute-Provence, 04870 Saint Michel l’Observatoire, France}
+\affil[d]{Aix Marseille Univ, CNRS/IN2P3, CPPM, Marseille, France}
 \affil[e]{LUPM, Université Montpellier \& CNRS, F-34095 Montpellier, France}
 \affil[f]{LPC, université Clermont Auvergne, CNRS, F-63000 Clermont-Ferrand, France}
-\affil[g]{Sorbonne Universit\'e, CNRS, Universit\'e de Paris, LPNHE, 75252 Paris Cedex 05, France}
-\affil[g]]{Department of Astronomy, Harvard University, 60 Garden St., Cambridge, MA 02138, USA}
 '''
+#\affil[g]{Sorbonne Universit\'e, CNRS, Universit\'e de Paris, LPNHE, 75252 Paris Cedex 05, France}
 import re
 
 def author_format(author, institutes):
@@ -50,13 +48,13 @@ def arxiv_format(author, institute):
 #print(institutes)
 #print(authors)
 if True:
-    print('\\author{')
-    print('\n \\and '.join([author_format(author, institutes) for author in authors]))
-    print('}')
-    
-    print('\\institute{')
-    print('\n \\and '.join([_i[1] for _i in institutes]))
-    print('}')
-if True:
+    with open('authors.tex', 'w') as fid:
+        print('\\author{', file=fid)
+        print('\n \\and '.join([author_format(author, institutes) for author in authors]), file=fid)
+        print('}', file=fid)
+        print('\\institute{', file=fid)
+        print('\n \\and '.join([_i[1] for _i in institutes]), file=fid)
+        print('}', file=fid)
+if False:
     print(', '.join([arxiv_format(author, institutes) for author in authors]))
     print(', '.join([f'({instlist.index(_i[0])}) {_i[1]}' for _i in institutes]))
